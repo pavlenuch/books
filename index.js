a832e9fd8dca0d4b8d055bad76dbc50ea6b612e3
Sashko <pavlenuch@yandex.by>
2021-12-08 23:08:47 +0300

    2308

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-const express = require('express')
-const app = express()
+import express from 'express'
+import mongoose from 'mongoose'
+import fileUpload from 'express-fileupload'
 
 const PORT = process.env.PORT || 80
+const DB_URL = `mongodb+srv://Alex:123@cluster0.2lffu.mongodb.net/books_fullstack?retryWrites=true&w=majority`
+
+const app = express()
+
+app.use(express.json())
+app.use(fileUpload({}))
+app.use(express.static('static'))
 
 app.get('/', (req, res)=>{
-    res.end('<h1>Home Page</h1>')
+    res.end('<h1>Home Page1</h1>')
 })
 
 app.get('/about', (req, res)=>{
     res.end('<h1>About Page</h1>')
 })
 
-app.listen(PORT, ()=>{
-    console.log('Server has been started...')
-})
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+            .then((res) => console.log('Connected to DB'))
+        app.listen(PORT, () => console.log('Server started on port', PORT))
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
